a02fe2b6943fb3a8682c43877396d5506728577d
mangoicecream <35483387+mangonihao@users.noreply.github.com>
2019-02-02 12:05:26 +0800

    尺取法

---
+++ 尺取法-Subsequence-O（n).cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<bitset>
+#include<stack>
+#include<set>
+#include<queue>
+#include<map>
+#include<cmath>
+#include<string>
+#include<cstring>
+#include<ctime>
+#include<fstream>
+#include<cstdlib>
+#include<algorithm>
+
+using namespace std;
+
+#define pii pair<int, int>
+#define pb push_back
+#define mem(a,b) memset(a,b,sizeof(a))
+#define per(i,a,b) for(int i=a;i<=b;i++)
+#define rep(i,a,b) for(int i=a;i>=b;i--)
+#define all(x) x.begin(),x.end()
+#define PER(i,x) for(auto i=x.begin();i!=x.end();i++)
+#define PI acos(-1.0)
+#define inf 0x3f3f3f3f
+typedef long long ll;
+const double eps=1.0e-5;
+const int maxn=2e5 + 10;
+
+int cnt1=0,cnt2=0,cnt3=0,n = 0,tmp = 0,a[maxn],b[maxn],c[maxn],ma=-inf,flag;
+int s = 0;
+int solve(){
+	int minv = n + 1;
+	int sum = 0 ,i = 1,e = 1;
+	while(true){
+		while(e <= n && sum < s){
+			sum += a[e++];
+			//sum += a[++e];int e = 0;while(e < n)
+		}
+		if(sum < s){
+			break;
+		}
+		minv = min(minv,e - i);
+		sum -= a[i++];
+	}
+	if(minv > n){
+		return 0;
+	}
+	return minv;
+}
+
+int main(){
+	int T = 0;
+	scanf("%d",&T); 
+	while(T--){
+		scanf("%d %d",&n,&s);
+		per(i,1,n){
+			scanf("%d",&a[i]);
+		}
+		printf("%d\n",solve());
+	}
+	
+	return 0; 
+}
