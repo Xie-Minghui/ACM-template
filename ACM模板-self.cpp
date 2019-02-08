8e23ac8d2a2ec73c45318de5446a1da7262f232f
mangoicecream <35483387+mangonihao@users.noreply.github.com>
2019-02-08 16:15:22 +0800

    ACM-template

---
+++ ACM模板-self.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<stack>
+#include<queue>
+#include<map>
+#include<cstring>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+typedef long long LL;
+
+#define INF 0x3f3f3f3f
+#define PI acos(-1.0)
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end()
+#define mem(a,b) memset(a,b,sizeof(a))
+#define per(i,a,b) for(int i = a;i <= b;++i)
+#define rep(i,a,b) for(int i = a;i >= b;--i)
+const int maxn = 3e5;
+int n = 0,m = 0; 
+void solve(){
+
+}
+int main(){
+	//std::ios::sync_with_stdio(false);
+	while(~scanf("%d",&n)){
+
+		solve();
+	}
+	
+	int T = 0;
+	scanf("%d",&T);
+	while(T--){
+		scanf("%d",&n);
+	}
+	
+	return 0;
+}
+
