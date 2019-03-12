57a27a066bf8e0ae480b741d43cbb6fa3106ac08
mangoicecream <35483387+mangonihao@users.noreply.github.com>
2019-03-12 15:04:04 +0800

    Update ACM模板-self.cpp

--- ACM模板-self.cpp
+++ ACM模板-self.cpp
@@ -12,24 +12,65 @@
 
 using namespace std;
 
+#define pii pair<int, int>
+#define mem(a,b) memset(a,b,sizeof(a))
+#define per(i,a,b) for(int i=(a);i<=(b);i++)
+#define rep(i,a,b) for(int i=(a);i>=(b);i--)
+#define pi acos(-1.0)
+#define inf 0x3f3f3f3f
+#define Rand() (rand()|rand()<<15)
+#define Srand() srand(time(0)^(unsigned)(unsigned long long)(new char))
+#define typemax(type)   numeric_limits<type>::max()
+#define typemin(type)   numeric_limits<type>::min()
 typedef long long LL;
 
-#define INF 0x3f3f3f3f
-#define PI acos(-1.0)
-#define pii pair<int,int>
-#define all(x) x.begin(),x.end()
-#define mem(a,b) memset(a,b,sizeof(a))
-#define per(i,a,b) for(int i = a;i <= b;++i)
-#define rep(i,a,b) for(int i = a;i >= b;--i)
-const int maxn = 3e5;
-int n = 0,m = 0; 
-void solve(){
+template<typename T> inline void read(T &x) {x = 0;int f = 1;char ch=getchar();while(ch<'0'||ch>'9') {if(ch=='-') f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}x=x*f;}
+template<typename E> E gcd(E a,E b){return b==0?a:gcd(b,a%b);}
+template<typename E> E lcm(E a,E b){return a*b/gcd(a,b);}
+template<typename E> E ex_gcd(E a,E b,E &x,E&y) {if(b==0){x=1;y=0;}else {ex_gcd(b,a%b,y,x);y-=(a/b)*x;}}
+template<typename E> E quick_mul(E a,E b,E c){E ans=0;while(b){if(b&1) ans=(ans+a)%c;a=(a+a)%c;b>>=1;}  return ans;}
+template<typename E> E quick_pow(E a,E b,E c){E ans=1;while(b){if(b&1) ans=ans*a%c;a=a*a%c;b>>=1;}return ans;}
+template<typename E> E inv1(E a,E b){return quick_pow(a,b-2,b);}
+template<typename E> E inv2(E a,E b){E x,y;ex_gcd(a,b,x,y);return (x%b+b)%b;}
 
+/*==================================Header Template=============================================*/
+//------------------------------------------Big integer I/O
+void scan(__int128 &x){//input
+	x = 0;
+	int f = 1;
+	char ch;
+	if((ch = getchar()) == '-'){
+		f = -1;
+	}else{
+		x = x * 10 + (ch - '0');
+	}
+	while((ch = getchar()) >= '0' && ch <= '9'){
+		x = x * 10 + (ch - '0');
+	}
+	x *= f;
+}
+void print(__int128 x){//output 
+	if(x < 0){
+		x = -x;
+		putchar('-');
+	}
+	if(x > 9){
+		print(x/10);//recursion
+	}
+	putchar(x%10+'0');
+}
+//-----------------------------------------
+const double eps=1.0e-5;
+const int maxn=200000+10;
+const LL mod=1e9+7;
+
+int n = 0,m = 0;
+void solve(){
+	
 }
 int main(){
 	//std::ios::sync_with_stdio(false);
 	while(~scanf("%d",&n)){
-
 		solve();
 	}
 	
@@ -38,7 +79,5 @@ int main(){
 	while(T--){
 		scanf("%d",&n);
 	}
-	
 	return 0;
 }
-
