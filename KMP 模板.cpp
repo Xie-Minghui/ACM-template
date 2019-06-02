3e5166539096750527e2edeaa8ec8a57c8fde8ab
mangoicecream <35483387+mangonihao@users.noreply.github.com>
2019-06-02 15:32:32 +0800

    解释

--- KMP 模板.cpp
+++ KMP 模板.cpp
@@ -7,18 +7,21 @@
 using namespace std;
 const int maxn = 1020;
 string str1,str2;
-int nex[maxn];
+int nex[maxn];//nex[i]表示nex[i]（除了本身）这个数的前面i个数
+//既是前缀又是后缀（注意不是回文）的最长字符串的长度（也就是该前缀
+//字符串的后一个位置，因为是s[i]匹配失败后，前nex[i]个字符已经知道可以直接
+//匹配好的，所以下一个比较的位置就是该前缀字符串的后一个位置
 
 void make_next(){
 	nex[0] = -1;
 	int j = 0,k = -1;
 	while(j < str2.size()){
 		if(k == -1 || str2[k] == str2[j]){
-			if(str2[k+1] == str2[j+1]){ //�������ַ����ʱҪ����
-				nex[j+1] = nex[k+1];	//��Ϊ���˵�kʱ����Ϊ��ȣ����Ƕ���� 
+			if(str2[k+1] == str2[j+1]){ //当两个字符相等时要跳过
+				nex[j+1] = nex[k+1];	//因为回退到k时，因为相等，就是多余的 
 				++j,++k;
 			}else{
-				nex[j+1] = k+1;//�����ʱ����1 
+				nex[j+1] = k+1;//不相等时，加1 
 				++j,++k;
 			}
 		}else{
