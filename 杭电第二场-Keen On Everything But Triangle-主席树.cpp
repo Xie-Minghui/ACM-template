9ee628ff04d2ee36881289412a31ad182c228e0f
mangoicecream <35483387+mangonihao@users.noreply.github.com>
2019-08-11 22:36:39 +0800

    Update 杭电第二场-Keen On Everything But Triangle-主席树.cpp
    添加一些注释

--- 杭电第二场-Keen On Everything But Triangle-主席树.cpp
+++ 杭电第二场-Keen On Everything But Triangle-主席树.cpp
@@ -7,8 +7,7 @@ using namespace std;
 const int N=500009,M=5000009;
 int P,a[N],b[N],rt[N],lc[M],rc[M],s[M];
 #define G c=getchar()
-inline void in(R&z)
-{
+inline void in(R&z){
     register bool f=0;
     register char G;
     while(c<'-')G;
@@ -17,20 +16,16 @@ inline void in(R&z)
     while(c>'-')z=(z<<3)+(z<<1)+(c&15),G;
     if(f)z=-z;
 }
-void build(R&t,R l,R r)
-{
+void build(R&t,R l,R r){
     t=++P;
-    if(l!=r)
-    {
+    if(l!=r){
         R m=(l+r)>>1;
         build(lc[t],l,m);
         build(rc[t],m+1,r);
     }
 }
-inline void insert(R*t,R u,R l,R r,R v)
-{
-    while(l!=r)
-    {
+inline void insert(R*t,R u,R l,R r,R v){
+    while(l!=r){
         s[*t=++P]=s[u]+1;//注意这里要+1
         R m=(l+r)>>1;
         if(v<=m)r=m,rc[*t]=rc[u],t=&lc[*t],u=lc[u];
@@ -38,10 +33,8 @@ inline void insert(R*t,R u,R l,R r,R v)
     }
     s[*t=++P]=s[u]+1;
 }
-inline long long ask(R t,R u,R l,R r,R k)
-{
-    while(l!=r)
-    {
+inline long long ask(R t,R u,R l,R r,R k){
+    while(l!=r){
         R m=(l+r)>>1,v=s[lc[u]]-s[lc[t]];
         if(k<=v)r=m,t=lc[t],u=lc[u];
         else  l=m+1,t=rc[t],u=rc[u],k-=v;
@@ -49,8 +42,7 @@ inline long long ask(R t,R u,R l,R r,R k)
     return b[l];
 }
 typedef long long ll;
-void init()
-{
+void init(){
     P=0;
     memset(a,0,sizeof(a));
     memset(b,0,sizeof(b));
@@ -59,11 +51,11 @@ void init()
     memset(rc,0,sizeof(rc));
     memset(s,0,sizeof(s));
 }
-int main()
-{
+int main(){
     R n,m,i,l,r,sz;
-    while(~scanf("%d%d",&n,&m))
-    {
+    string s1 = "000001",s2 = " 001010";
+    cout << (s1 > s2) << endl;
+    while(~scanf("%d%d",&n,&m)){
         init();
         for(i=1;i<=n;++i)
             in(a[i]),b[i]=a[i];
@@ -72,15 +64,16 @@ int main()
         build(rt[0],1,sz);
         for(i=1;i<=n;++i)
             insert(&rt[i],rt[i-1],1,sz,lower_bound(b+1,b+sz+1,a[i])-b);
-        while(m--)
-        {
+
+//------------上面就是主席树封装的模板--------------------------------------------
+
+        while(m--){
             in(l);in(r);int yy=r-l-1;
             if(r-l+1<3) {printf("-1\n");continue;}
-            ll a1=ask(rt[l-1],rt[r],1,sz,r-l+1);
+            ll a1=ask(rt[l-1],rt[r],1,sz,r-l+1);//查询区间[l,r]的第r-l+1小的数
             ll a2=ask(rt[l-1],rt[r],1,sz,r-l);
             ll a3=ask(rt[l-1],rt[r],1,sz,r-l-1);
-            while(a2+a3<=a1&&yy>1)//y >= 1也可以，后面有yy=0的判断条件
-            {
+            while(a2+a3<=a1&&yy>1){//y >= 1也可以，后面有yy=0的判断条件
                 a1=a2;a2=a3;a3=ask(rt[l-1],rt[r],1,sz,yy-1);
                 yy--;
             }
